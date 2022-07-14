3ac1716583e5b0707ac40296dd772254d4e36d60
WikkiT <108848834+WikkiTang@users.noreply.github.com>
2022-07-14 17:01:28 -0400

    Update 8 7458.sv

--- 2 Verilog language/1 Basics/8 7458.sv
+++ 2 Verilog language/1 Basics/8 7458.sv
@@ -5,12 +5,15 @@ module top_module (
     output p2y );
     wire  w1,w2,w3,w4;
     
-assign w1=p2a&p2b;
-assign w2=p2c&p2d;
-assign p2y=w1|w2;
+    /*assign w1=p2a&p2b;
+    assign w2=p2c&p2d;
+    assign p2y=w1|w2;
     
-assign w3=p1c&p1a&p1b;
-assign w4=p1f&p1e&p1d;
-assign p1y=w3|w4;
+    assign w3=p1c&p1a&p1b;
+    assign w4=p1f&p1e&p1d;
+    assign p1y=w3|w4;*/
+    
+    assign p2y=(p2a&p2b)|(p2c&p2d);
+    assign p1y=(p1c&p1a&p1b)|(p1f&p1e&p1d);
 
 endmodule
